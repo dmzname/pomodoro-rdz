0f7a3f87551b5ad3c543a8ffd8d8eca665f8642f
Dmitry Zabelin <zabelin.info@gmail.com>
2023-07-27 17:27:19 +0300

    Added unit testing: Jest, React testing library.

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,39 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ButtonHTMLAttributes, FC } from 'react';
+import cls from './Button.module.scss';
+
+export enum ButtonTheme {
+    CLEAR = 'clear',
+    OUTLINE = 'outline',
+    BACKGROUND = 'background',
+}
+
+export enum ButtonSize {
+    L = 'size-l',
+    M = 'size-m',
+    XL = 'size-xl',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string;
+    theme?: ButtonTheme;
+    square?: boolean;
+    size?: ButtonSize;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+    const { className, children, theme, square, size = ButtonSize.M, ...otherProps } = props;
+
+    const mods: Record<string, boolean> = {
+        [cls.square]: square,
+    };
+
+    return (
+        <button
+            type='button'
+            className={classNames(cls.root, mods, [className, cls[theme], cls[size]])}
+            {...otherProps}>
+            {children}
+        </button>
+    );
+};
